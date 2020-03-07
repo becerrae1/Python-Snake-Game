33b8e08385ee3ada8dc849c2510c71df16630546
Ethan Medrano <ethanmedrano52@gmail.com>
2020-03-06 21:43:27 -0500

    update

--- game.py
+++ game.py
@@ -54,6 +54,16 @@ class snake(object):
             p = c.pos[:]
             if p in self.turns:
                 turn = self.turns[p]
+                c.move(turn[0],turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows-1)
+                else: c.move(c.dirnx,c.dirny)
+
     def reset(self, pos):
         pass
 
@@ -61,7 +71,11 @@ class snake(object):
         pass
 
     def draw(self, surface):
-        pass
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
 
 def drawGrid(w, rows, surface):
     sizeBtwn = w // rows
